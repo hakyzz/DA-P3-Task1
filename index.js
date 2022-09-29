d6cc3c55b171abd2fcf0816df1143f1b98a1dafe
Hakan Arioglu <hakan.arioglu@namics.com>
2022-09-29 19:01:54 +0200

    finish last task and do some refactorings

--- index.js
+++ index.js
@@ -1,24 +1,86 @@
-const solanaWeb3 = require('@solana/web3.js');
+import { createMint, getOrCreateAssociatedTokenAccount, mintTo, transfer } from '@solana/spl-token';
+import { Keypair, Connection, LAMPORTS_PER_SOL, Transaction, SystemProgram, sendAndConfirmTransaction, PublicKey } from '@solana/web3.js';
 
 const secret = new Uint8Array([192,147,150,141,14,153,106,37,250,223,102,102,117,36,197,3,47,111,222,220,45,74,11,190,182,150,84,71,180,98,198,155,161,83,51,226,155,30,67,60,197,26,97,224,208,198,251,144,41,160,209,92,120,177,73,206,91,99,210,106,149,49,29,32]);
-const keypair = solanaWeb3.Keypair.fromSecretKey(secret);
+const keypair = Keypair.fromSecretKey(secret);
 const rpcUrl = "https://api.devnet.solana.com";
 
-const connection = new solanaWeb3.Connection(rpcUrl);
+const connection = new Connection(rpcUrl);
+
+const toWallet = new PublicKey("HtUaVzWiSNrrY2NSVKroE3883vnBfn8SMrLM2UxA2vDy");
 
 async function transferTask() {
-    await connection.requestAirdrop(keypair.publicKey, solanaWeb3.LAMPORTS_PER_SOL);
-    let transaction = new solanaWeb3.Transaction();
+    await connection.requestAirdrop(keypair.publicKey, LAMPORTS_PER_SOL);
+    let transaction = new Transaction();
 
     transaction = transaction.add(
-        solanaWeb3.SystemProgram.transfer({
+        SystemProgram.transfer({
             fromPubkey: keypair.publicKey,
-            toPubkey: "HtUaVzWiSNrrY2NSVKroE3883vnBfn8SMrLM2UxA2vDy",
-            lamports: solanaWeb3.LAMPORTS_PER_SOL
+            toPubkey: toWallet,
+            lamports: LAMPORTS_PER_SOL
         })
     );
 
-    await solanaWeb3.sendAndConfirmTransaction(connection, transaction, [keypair]);
+    await sendAndConfirmTransaction(connection, transaction, [keypair]);
+}
+
+async function instructionsTask() {
+    let transaction = new Transaction();
+
+    transaction = transaction.add(
+        SystemProgram.transfer({
+            fromPubkey: keypair.publicKey,
+            toPubkey: toWallet,
+            lamports: LAMPORTS_PER_SOL / 10
+        })
+    );
+
+    let solTransferHash = await sendAndConfirmTransaction(connection, transaction, [keypair]);
+    console.log("Sol transfer: ", solTransferHash);
+
+    const mint = await createMint(
+        connection,
+        keypair,
+        keypair.publicKey,
+        null,
+        9
+    );
+
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        keypair,
+        mint,
+        keypair.publicKey
+    )
+
+    const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection, 
+        keypair, 
+        mint, 
+        toWallet
+    );
+
+    let mintSignature = await mintTo(
+        connection,
+        keypair,
+        mint,
+        tokenAccount.address,
+        keypair.publicKey,
+        100000000000 
+    )
+    console.log('Mint tx:', mintSignature);
+
+    let tokenTransfer = await transfer(
+        connection,
+        keypair,
+        tokenAccount.address,
+        toTokenAccount.address,
+        keypair.publicKey,
+        LAMPORTS_PER_SOL / 10
+    )
+
+    console.log("Token Transfer: ", tokenTransfer);
 }
 
 transferTask();
+instructionsTask();
